8eabe4d272a050ff2fcff1ce9f1dd08c49b7740b
Ashley Carter <acart052@odu.edu>
2023-11-18 22:36:43 -0500

    put calcs into separate class

---
+++ CPUSCHED/Scheduler.java
@@ -0,0 +1,25 @@
+package CPUSCHED;
+
+import java.text.DecimalFormat;
+
+public abstract class Scheduler 
+{
+    protected void printCalc(int startTime, int endTime, int currTime, int numProcess, int totalWaitTime, 
+    int totalTurnaroundTime, int totalResponseTime, int totalProcessingTime)
+    {
+        double throughput = (double)numProcess / currTime;
+        double cpuUtilization = ((double)totalProcessingTime / (currTime - startTime)) * 100;
+        double avgWaitTime = (double)totalWaitTime / numProcess;
+        double avgTurnaroundTime = (double)totalTurnaroundTime / numProcess;
+        double avgResponseTime = (double)totalResponseTime / numProcess;
+        DecimalFormat df = new DecimalFormat("#.##");
+
+        System.out.println("Total elaspted time: " + df.format(currTime));
+        System.out.println("Throughput: " + df.format(throughput));
+        System.out.println("CPU utilization: "  + df.format(cpuUtilization) + "%");
+        System.out.println("Average wait time (in CPU burst times): " + df.format(avgWaitTime));
+        System.out.println("Average turnaround time (in CPU burst times): " + df.format(avgTurnaroundTime));
+        System.out.println("Average response time (in CPU burst times): " + df.format(avgResponseTime));
+    }
+    
+}
