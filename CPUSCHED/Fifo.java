1a0f95066565325dc31f2f256c174e65a02e6a38
Ashley Carter <acart052@odu.edu>
2023-11-18 22:50:09 -0500

    removed commented code

--- CPUSCHED/Fifo.java
+++ CPUSCHED/Fifo.java
@@ -39,21 +39,6 @@ public class Fifo extends Scheduler
         }
 
         printCalc(startTime, currTime, numProcess, totalWaitTime, 
-        totalTurnaroundTime, totalResponseTime, totalProcessingTime);
-        
-        // double throughput = (double)numProcess / currTime;
-        // double cpuUtilization = ((double)totalProcessingTime / (currTime - startTime)) * 100;
-        // double avgWaitTime = (double)totalWaitTime / numProcess;
-        // double avgTurnaroundTime = (double)totalTurnaroundTime / numProcess;
-        // double avgResponseTime = (double)totalResponseTime / numProcess;
-        // DecimalFormat df = new DecimalFormat("#.##");
-
-        // System.out.println("Total elaspted time: " + df.format(currTime));
-        // System.out.println("Throughput: " + df.format(throughput));
-        // System.out.println("CPU utilization:"  + df.format(cpuUtilization) + "%");
-        // System.out.println("Average wait time (in CPU burst times): " + df.format(avgWaitTime));
-        // System.out.println("Average turnaround time (in CPU burst times): " + df.format(avgTurnaroundTime));
-        // System.out.println("Average response time (in CPU burst times): " + df.format(avgResponseTime));
-        
+                    totalTurnaroundTime, totalResponseTime, totalProcessingTime);
     }
 }
