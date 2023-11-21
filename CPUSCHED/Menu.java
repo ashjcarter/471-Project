d7758135826505b7d46a4b8d7d009c458aa31f1e
Ashley Carter <acart052@odu.edu>
2023-11-21 03:02:19 -0500

    created user menu

---
+++ CPUSCHED/Menu.java
@@ -0,0 +1,20 @@
+package CPUSCHED;
+
+import java.util.Scanner;
+
+public class Menu 
+{ public int display()
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Select a scheduling algorithm:");
+        System.out.println("1. First Come First Serve");
+        System.out.println("2. Shortest Job First");
+        System.out.println("3. Priority(non preemptive)");
+
+        int choice = scanner.nextInt();
+        scanner.close();       
+
+        return choice;
+    }
+    
+}
