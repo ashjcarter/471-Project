a0ec742265bce9094e81d852f7990bb2fd7a0235
Ashley <acart052@odu.edu>
2023-11-22 03:02:50 -0500

    added documentation

--- cpusched/Sjf.java
+++ cpusched/Sjf.java
@@ -7,16 +7,31 @@ import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
+/**
+ * This class implements the Shortest Job First (SJF) Scheduling algorithm.
+ * Extends the Calculations class to generate statistics based on the SJF Scheduling.
+ */
 public class Sjf extends Calculations
 {
-    
+    /**
+     * Executes the Shortest Job First (SJF) Scheduling algorithm on the provided process queue
+     * and generates statistics based on the scheduling.
+     *
+     * @param processQueue The queue of processes to be scheduled using SJF.
+     * @return A string containing the calculated statistics.
+     */
     public String sjfScheduling(Queue<Process> processQueue)
     {
-        List<Process> processes = new ArrayList<>(processQueue);
-        Collections.sort(processes, Comparator.comparingInt(Process::getArrivalTime));
+        // Convert the process queue to a list for sorting by arrival time
+        List<Process> processList = new ArrayList<>(processQueue);
+        Collections.sort(processList, Comparator.comparingInt(Process::getArrivalTime));
+
+        // Priority queue to store processes in order of burst time
         PriorityQueue<Process> readyQueue = new PriorityQueue<>(Comparator.comparingInt(Process::getBurstUnits));
         
+        // Initialize variables to store statistics
         int numProcess = processQueue.size();
+        int turnaroundTime = 0;
         int totalWaitTime = 0;
         int totalTurnaroundTime = 0;
         int totalResponseTime = 0;
@@ -25,46 +40,51 @@ public class Sjf extends Calculations
         int currTime = 0;
         int index = 0;
 
+        // Process the ready queue until all processes are executed
         while(!readyQueue.isEmpty() || index < numProcess)
         {
-            while(index < numProcess && processes.get(index).getArrivalTime() <= currTime)
+            // Add processes to ther ready queue that have arrived at the current time
+            while(index < numProcess && processList.get(index).getArrivalTime() <= currTime)
             {
-                readyQueue.offer(processes.get(index++));
+                readyQueue.offer(processList.get(index++));
             }
         
-
+        // If the ready queue is not empty
         if(!readyQueue.isEmpty())
         {
-            
+            // Process the shortest job in the ready queue
             Process currProcess = readyQueue.poll();
+
+            // If the process has arrived and is ready to execute
             if(currProcess.getArrivalTime() <= currTime)
             {
+                // Calculate the statistics
                 waitTime = currTime - currProcess.getArrivalTime();
                 currTime += currProcess.getBurstUnits();
+                totalWaitTime += waitTime;
+                totalResponseTime += waitTime;
+                totalProcessingTime += currProcess.getBurstUnits();
+                totalTurnaroundTime += waitTime + currProcess.getBurstUnits();
             }
             else
             {
+                // Else the process arrives after the current time, 
+                // So start its execution when it arrives
                 currTime = currProcess.getArrivalTime() + currProcess.getBurstUnits();
             }
-
-            totalResponseTime += waitTime;
-
-            totalProcessingTime += currProcess.getBurstUnits();
-
-            totalWaitTime += waitTime;
-
-            int turnAroundTime = waitTime + currProcess.getBurstUnits();
-            totalTurnaroundTime += turnAroundTime;
-
         }
         else
         {
-            currTime = processes.get(index).getArrivalTime();
+            // Else no processes are ready, move time forward 
+            // To the next process arrival time
+            currTime = processList.get(index).getArrivalTime();
         }
     }
+    
+    // Create an instance of the Calculations class to generate and return statistics
     Calculations calc = new Calculations();
     return calc.generateStats(currTime, numProcess, totalWaitTime, 
-               totalTurnaroundTime, totalResponseTime, totalProcessingTime);
+                              totalTurnaroundTime, totalResponseTime, totalProcessingTime);
         
     }
     
