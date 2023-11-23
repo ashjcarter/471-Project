1fcc643d4cfb16b312088a223e7682658c9ff8d2
Ashley Carter <acart052@odu.edu>
2023-11-23 01:07:59 -0500

    added documentation

--- producer_consumer/Menu.java
+++ producer_consumer/Menu.java
@@ -4,8 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * This class provides a menu for user to select an input file.
+ */
 public class Menu 
 {
+    /**
+     * This method allows the user to select an input file from a given list 
+     * and returns the corresponding input and output file names.
+     * 
+     * @return A List of Strings where the first element is the input file name 
+     *         and the second element is the output file name.
+     */
     @SuppressWarnings("resource")
     public static List<String> selectFile()
     {
@@ -21,28 +31,39 @@ public class Menu
         String inFile = "";
         String outFile = "";
 
+        // Switch case to handle user input
         switch(choice)
         {
+            // Read from Input-4sec-Wait.txt and write to Output-4sec-Wait.txt
             case 1:
                 inFile = "producer_consumer/Input-4sec-Wait.txt";
                 outFile = "producer_consumer/Output-4sec-Wait.txt";
                 break;
+
+            // Read from Input-6sec-Wait.txt and write to Output-6sec-Wait.txt
             case 2:
                 inFile = "producer_consumer/Input-6sec-Wait.txt";
                 outFile = "producer_consumer/Output-6sec-Wait.txt";
                 break;
+
+            // Read from Input-8sec-Wait.txt and write to Output-8sec-Wait.txt
             case 3:
                 inFile = "producer_consumer/Input-8sec-Wait.txt";
                 outFile = "producer_consumer/Output-8sec-Wait.txt";
                 break;
+
+            // Default case returns Input-4sec-Wait.txt and write to Output-4sec-Wait.txt
             default:
                 System.out.println("Invalid choice. Using default input file.");
                 inFile = "producer_consumer/Input-4sec-Wait.txt";
                 outFile = "producer_consumer/Output-4sec-Wait.txt";
                 break;
         }
+        // Add input and output file names to the list
         files.add(inFile);
         files.add(outFile);
+
+        // Return the list of input and output file names
         return files;
     }
 }
