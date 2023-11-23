1ce7fabd4b05b73e6f6952d6b81c8a665f113efc
Ashley Carter <acart052@odu.edu>
2023-11-22 21:49:53 -0500

    added documentation

--- producer_consumer/Item.java
+++ producer_consumer/Item.java
@@ -1,22 +1,41 @@
 package producer_consumer;
 
+/**
+ * This class represents an item produced by the producer in the Producer-Consumer problem.
+ */
 public class Item 
 {
-    private final int value;
-    private final long productionTime;
+    private final int randItem; // The randomly generated number for producers to produce
+    private final long productionTime; // The time at which the item was produced
 
+    /**
+     * Constructs a new Item with the given value and sets the production time.
+     *
+     * @param value The generated number of the item
+     */
     public Item(int value) 
     {
-        this.value = value;
-        this.productionTime = System.currentTimeMillis();
+        this.randItem = value;
+        this.productionTime = System.currentTimeMillis(); 
     }
 
-    public int getValue() 
+    /**
+     * Returns the randomly generated item value of this item.
+     *
+     * @return The randomly generated item value of this item
+     */
+    public int getRandItem() 
     {
-        return value;
+        return randItem;
     }
 
-    public double getProductionTime() 
+    /**
+     * Returns the production time of this item.
+     * The production time is represented as the number of milliseconds 
+     *
+     * @return The production time of this item
+     */
+    public long getProductionTime() 
     {
         return productionTime;
     }
