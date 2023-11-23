ce8e4896fbdd3f88aba2b9e7e7215cd776a554fe
Ashley Carter <acart052@odu.edu>
2023-11-23 00:55:13 -0500

    added documentation

--- producer_consumer/Main.java
+++ producer_consumer/Main.java
@@ -7,41 +7,55 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+/**
+ * The Main class that simulates the producer-consumer problem using Java threads.
+ * It creates producers and consumers according to the test cases read from a file. 
+ */
 public class Main
 {
-    private static final int bufferSize = 10;
+    private static final int bufferSize = 5;
     private static final BlockingQueue<Item> buffer = new LinkedBlockingQueue<>(bufferSize);
+
+    // Flag to control the exectuion of producer and consumer threads
     private static final AtomicBoolean runThreads = new AtomicBoolean(true);
-    private static final Random rand = new Random();
-    private static int sleepTime = 1;
-    private static long totalTurnaroundTime = 0;
+
+    private static int sleepTime = 1; // Default sleep time override within runThreads()
+    private static double totalTurnaroundTime = 0;
     private static int totalItems = 0;
 
+    /**
+     * Producer logic: Generates items and inserts them into the buffer.
+     */
     private static void producer()
     {
-        // Logic to produce items and insert into buffer
+        Random rand = new Random();
+        // Keep producting items and insert into buffer until falg is set to false
         while(runThreads.get()) 
         {
             try
             {
                 Thread.sleep(sleepTime);
+                // Generate random item to be stored into the buffer
                 int randItem = rand.nextInt(100);
+                // Creating an item with the random value
                 Item item = new Item(randItem);
                 buffer.put(item);
-                System.out.println("Producer produced an item: " + item);
 
             }
             catch(InterruptedException e)
             {
-                System.out.println("Producer interrupted.");
+                // System.out.println("Producer interrupted.");
                 break;
             }
         }
     }
 
+    /**
+     * Consumer logic: Consumes items from the buffer and calculates their turnaround time.
+     */
     private static void consumer()
     {
-        // Logic to consume items and remove from buffer
+        // Keep consuming items until the flag is set to false and the buffer is empty
         while(runThreads.get() || !buffer.isEmpty()) 
         {
             try
@@ -50,9 +64,6 @@ public class Main
                 double turnaroundTime = System.currentTimeMillis() - item.getProductionTime();
                 totalTurnaroundTime += turnaroundTime;
                 totalItems++;
-                System.out.println("Consumer consumed an item: " + item);
-                System.out.println("Current turnaround time: " + turnaroundTime);
-                System.out.println("Current total items: " + totalItems);
                 Thread.sleep(sleepTime);
             }
             catch(InterruptedException e)
@@ -64,16 +75,22 @@ public class Main
         }
     }
 
+    /**
+     * Runs the producer and consumer threads based on the test case configuration.
+     *
+     * @param testCase The test case defining the simulation parameters.
+     */
     public static void runThreads(TestCase testCase)
     {
-        List<Thread> threads = new ArrayList<>();
+        List<Thread> threads = new ArrayList<>(); // List to hold the producer and consumer threads
         int numProducers = testCase.getNumProducers();
         int numConsumers = testCase.getNumConsumers();
-        int runThreadsSleep = sleepTime * 100;
+        int runThreadsSleep = sleepTime * 100; // Sleep time for the simulation 
     
-        sleepTime = testCase.getSleepTime();
-        runThreads.set(true);
+        sleepTime = testCase.getSleepTime(); // Sleep time for producer and consumer threads
+        runThreads.set(true); // Flag to control the exectuion of producer and consumer threads
 
+        // Create and start the producer thread
         for(int i = 0; i < numProducers; i++)
         {
             Thread producerThread = new Thread(Main::producer);
@@ -81,6 +98,7 @@ public class Main
             threads.add(producerThread);
         }
 
+        // Create and start the consumer thread 
         for(int j = 0; j < numConsumers; j++)
         {
             Thread consumerThread = new Thread(Main::consumer);
@@ -91,22 +109,24 @@ public class Main
         try
         {
             Thread.sleep(runThreadsSleep);
-            runThreads.set(false);
+            runThreads.set(false); // Set flag to false to stop producer and consumer threads
 
+            // Interrupt all threads
             for(Thread thread : threads)
             {
                 thread.interrupt();
             }
 
+            // Wait for all threads to finish
             for(Thread thread : threads)
             {
                 thread.join();
             }
 
-            double avgTurnaroundTime = (double)totalTurnaroundTime / totalItems ;
+            double avgTurnaroundTime = totalTurnaroundTime / (double)totalItems ;
 
             WriteFile.writeFile(testCase.getOutFile(), testCase.getTestCaseID(), 
-            sleepTime, numProducers, numConsumers, avgTurnaroundTime);
+                                sleepTime, numProducers, numConsumers, avgTurnaroundTime);
         }
         catch(InterruptedException e)
         {
@@ -115,22 +135,33 @@ public class Main
         
     }
 
+    /**
+     * Main method: Reads input files and runs simulations.
+     *
+     * @param args Command-line arguments(unused in this case).
+     */
     public static void main(String[] args)
     {
         List<String> files = Menu.selectFile();
+
+        // Getting input and output file names from the list
         String inFile = files.get(0);
         String outFile = files.get(1);
 
         System.out.println("You've chosen " + inFile);
         System.out.println("Running simulation...");
 
+        // Calling the readFile method to read the input file and create a list of test cases
         List<TestCase> testCases = ReadFile.readFile(inFile);
+
+        // Looping through each test case, running the simulation 
+        //and writing the output to the output file
         for(TestCase testCase : testCases)
         {
             testCase.setOutFile(outFile);
             runThreads(testCase);
         }
-        System.out.println("Simulation completed. Check the output file.");
+        System.out.println("Simulation completed. Check the output file" + outFile + ".");
 
     }
 
